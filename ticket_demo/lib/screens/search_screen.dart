6b3123235d2450ad396b57a645df992b3e5ff215
Minn05 <minh.nhan.tran2001@gmail.com>
2023-02-13 21:04:12 +0700

    ticket_screen

--- ticket_demo/lib/screens/search_screen.dart
+++ ticket_demo/lib/screens/search_screen.dart
@@ -6,6 +6,7 @@ import 'package:ticket_demo/utils/app_layout.dart';
 import 'package:ticket_demo/utils/app_styles.dart';
 import 'package:ticket_demo/widget/double_text_widget.dart';
 import 'package:ticket_demo/widget/icon_text_widget.dart';
+import 'package:ticket_demo/widget/ticket_tab.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -28,48 +29,7 @@ class SearchScreen extends StatelessWidget {
                 .copyWith(fontSize: AppLayout.getHeight(35)),
           ),
           Gap(AppLayout.getHeight(20)),
-          FittedBox(
-            child: Container(
-              padding: const EdgeInsets.all(3.5),
-              child: Row(
-                children: [
-                  //Airline tickets
-                  Container(
-                    width: size.width * .44,
-                    padding:
-                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.horizontal(
-                          left: Radius.circular(AppLayout.getHeight(50))),
-                      color: Colors.white,
-                    ),
-                    child: Center(
-                      child: Text("Airline tickets"),
-                    ),
-                  ),
-                  //Hotels
-                  Container(
-                    width: size.width * .44,
-                    padding:
-                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.horizontal(
-                        right: Radius.circular(AppLayout.getHeight(50)),
-                      ),
-                      color: Colors.transparent,
-                    ),
-                    child: Center(
-                      child: Text("Hotels"),
-                    ),
-                  ),
-                ],
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
-                color: const Color(0xFFF4F6FD),
-              ),
-            ),
-          ),
+          const AppTicketTabs(firstTab: "Airline tickets", secondTab: "Hotels"),
           Gap(AppLayout.getHeight(25)),
           const AppIconText(
               icon: Icons.flight_takeoff_rounded, text: "Departure"),
---
+++ ticket_demo/lib/screens/ticket_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:gap/gap.dart';
+import 'package:ticket_demo/utils/app_layout.dart';
+import 'package:ticket_demo/utils/app_styles.dart';
+
+class TicketScreen extends StatelessWidget {
+  const TicketScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: Stack(
+        children: [
+          ListView(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppLayout.getHeight(20),
+              vertical: AppLayout.getHeight(20),
+            ),
+            children: [
+              Gap(AppLayout.getHeight(40)),
+              Text(
+                "Tickets",
+                style: Styles.headLineStyle1,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
